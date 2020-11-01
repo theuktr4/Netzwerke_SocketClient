33b5af1b2b562eb35b6fe264e9025f942798787b
hm-haerting <lukas.haertinger@hm.edu>
2020-11-01 10:41:43 +0100

    Encoding

--- src/FreeDiskSpaceClient.java
+++ src/FreeDiskSpaceClient.java
@@ -14,7 +14,7 @@ public class FreeDiskSpaceClient {
         try (Socket socket = new Socket(ip, PORT)) {
             System.out.printf("Verbunden mit %s: %s%n", ip, PORT);
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), CHARSET_NAME));
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream(), CHARSET_NAME));
             //Anfrage senden
             out.write(path);
             out.newLine();
